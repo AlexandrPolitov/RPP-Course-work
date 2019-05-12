a9d2572c3f299784b2fe32e126c716ec9ad72e1b
dmitry <dmitri.lisen@mail.ru>
2019-05-12 14:50:10 +0300

    SideBar

--- CourseWork/app/src/main/java/com/e/myapplication/MainActivity.java
+++ CourseWork/app/src/main/java/com/e/myapplication/MainActivity.java
@@ -3,10 +3,14 @@ package com.e.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.e.myapplication.Net.NetInterface;
 import com.e.myapplication.pojo.Events;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,12 +29,21 @@ public class MainActivity extends AppCompatActivity {
     private Retrofit retrofit;
     private Events events;
 
+    private ListView navigationList;
+    private String[] navigationItems;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationItems = getResources().getStringArray(R.array.dima_sanya_go);
+        navigationList = findViewById(R.id.left_drawer);
+
+        navigationList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item, navigationItems));
+
 
         retrofit = new Retrofit.Builder().baseUrl(URL_JSON)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -42,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Events> call, Response<Events> response) {
                 events = response.body();
                 if (!events.equals(null))
-                      Log.e("my", "*** lvl: " + String.valueOf(events.getCount()));
+                      Log.e("my", "*** lvдl: " + String.valueOf(events.getCount()));
             }
 
             @Override
